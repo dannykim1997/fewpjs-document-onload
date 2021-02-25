628bf055c89ad97e535a43538fdd9efb3e1c986d
github username <github email address>
2021-02-24 18:20:09 -0800

    commit tree 163eab3bf5989395aa00cdb57ed32fad493b425b

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    return newMessage()
+} )
+
+function newMessage() {
+    document.querySelector('p').innerHTML = 'This is really cool!'
+}
